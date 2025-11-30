1c18fda0c5b30ecba4e39c7e475593ab59dee505
Aditya som <ADITYASOAM55@GMAIL.COM>
2025-11-30 13:14:31 +0530

    User model created

---
+++ models/User.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // hashed password
+  avatar: { type: String, default: "" },
+  channels: { type: [String], default: [] },
+});
+
+module.exports = mongoose.model("Users", UserSchema);
