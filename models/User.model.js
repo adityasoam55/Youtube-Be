0b95df4e3c1a047b5045fb447265630c95d8e006
Aditya som <ADITYASOAM55@GMAIL.COM>
2025-12-04 16:13:07 +0530

    Option to add channel description provided

--- models/User.model.js
+++ models/User.model.js
@@ -9,6 +9,8 @@ const UserSchema = new mongoose.Schema({
   avatar: { type: String, default: "https://i.pravatar.cc/150" },
   banner: { type: String, default: "" },
 
+  channelDescription: { type: String, default: "" },
+
   channels: { type: [String], default: [] },
 });
 
